7fdb22d5d37b31c64bdb70864ff6a331f8b03f17
Maciek <msitkowski94@gmail.com>
2021-11-11 07:38:32 +0100

    update prettier config and format all files

--- svelte.config.js
+++ svelte.config.js
@@ -5,22 +5,22 @@ const isDev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		paths: {
-			base: isDev ? '' : '/what-to-do'
-		}
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+    }),
+    paths: {
+      base: isDev ? '' : '/what-to-do',
+    },
+  },
 };
 
 export default config;
